20108bbc9802fd4ce744f34f311dec3529cc4a9d
ivaylo <ivaylo.rumenov@gmail.com>
2022-02-10 13:14:26 +0200

    upr. 1

--- upr1.js
+++ upr1.js
@@ -3,3 +3,17 @@ function add(a,b) {
 }
 console.log (add)
 
+function add1(a,b) {
+    return a - b
+}
+console.log (add1)
+
+function add2(a,b) {
+    return a / b
+}
+console.log (add2)
+
+function add3(a,b) {
+    return a * b
+}
+console.log (add3)
